e3fa31a22f89cfefda2b959251f7f91e7bd2167c
sofihoyos <169062995+sofihoyos@users.noreply.github.com>
2024-11-13 09:15:19 -0500

    countries.js

---
+++ scripts/countries.js
@@ -0,0 +1,155 @@
+export const countries = [
+    {
+        name: "Argentina",
+        capital: "Buenos Aires",
+        population: 45195777,
+        currency: "Peso",
+        language: "Spanish",
+        flag: "assets/images/flags/Argentina.png"
+    },
+    {
+        name: "Bolivia",
+        capital: "Sucre",
+        population: 11673021,
+        currency: "Boliviano",
+        language: "Spanish, Quechua, Aymara",
+        flag: "assets/images/flags/Bolivia.png"
+    },
+    {
+        name: "Brazil",
+        capital: "Brasília",
+        population: 213993437,
+        currency: "Real",
+        language: "Portuguese",
+        flag: "assets/images/flags/Brasil.png"
+    },
+    {
+        name: "Chile",
+        capital: "Santiago",
+        population: 19116201,
+        currency: "Peso",
+        language: "Spanish",
+        flag: "assets/images/flags/Chile.png"
+    },
+    {
+        name: "Colombia",
+        capital: "Bogotá",
+        population: 50882891,
+        currency: "Peso",
+        language: "Spanish",
+        flag: "assets/images/flags/Colombia.jpeg"
+    },
+    {
+        name: "Costa Rica",
+        capital: "San José",
+        population: 5094118,
+        currency: "Colón",
+        language: "Spanish",
+        flag: "assets/images/flags/CostaRica.png"
+    },
+    {
+        name: "Cuba",
+        capital: "Havana",
+        population: 11326616,
+        currency: "Peso",
+        language: "Spanish",
+        flag: "assets/images/flags/Cuba.png"
+    },
+    {
+        name: "Dominican Republic",
+        capital: "Santo Domingo",
+        population: 10847904,
+        currency: "Peso",
+        language: "Spanish",
+        flag: "assets/images/flags/RDominicana.jpeg"
+    },
+    {
+        name: "Ecuador",
+        capital: "Quito",
+        population: 17643060,
+        currency: "Dollar",
+        language: "Spanish",
+        flag: "assets/images/flags/Ecuador.png"
+    },
+    {
+        name: "El Salvador",
+        capital: "San Salvador",
+        population: 6486201,
+        currency: "Dollar",
+        language: "Spanish",
+        flag: "assets/images/flags/ElSalvador.png"
+    },
+    {
+        name: "Guatemala",
+        capital: "Guatemala City",
+        population: 17915568,
+        currency: "Quetzal",
+        language: "Spanish",
+        flag: "assets/images/flags/Guatemala.png"
+    },
+    {
+        name: "Honduras",
+        capital: "Tegucigalpa",
+        population: 10269129,
+        currency: "Lempira",
+        language: "Spanish",
+        flag: "assets/images/flags/Honduras.png"
+    },
+    {
+        name: "Mexico",
+        capital: "Mexico City",
+        population: 126190788,
+        currency: "Peso",
+        language: "Spanish",
+        flag: "assets/images/flags/Mexico.png"
+    },
+    {
+        name: "Nicaragua",
+        capital: "Managua",
+        population: 6624554,
+        currency: "Córdoba",
+        language: "Spanish",
+        flag: "assets/images/flags/Nicaragua.png"
+    },
+    {
+        name: "Panama",
+        capital: "Panama City",
+        population: 4314767,
+        currency: "Balboa, Dollar",
+        language: "Spanish",
+        flag: "assets/images/flags/Panama.png"
+    },
+    {
+        name: "Paraguay",
+        capital: "Asunción",
+        population: 7132537,
+        currency: "Guaraní",
+        language: "Spanish, Guaraní",
+        flag: "assets/images/flags/Paraguay.png"
+    },
+    {
+        name: "Peru",
+        capital: "Lima",
+        population: 33144926,
+        currency: "Sol",
+        language: "Spanish",
+        flag: "assets/images/flags/Peru.png"
+    },
+    {
+        name: "Uruguay",
+        capital: "Montevideo",
+        population: 3483000,
+        currency: "Peso",
+        language: "Spanish",
+        flag: "assets/images/flags/Uruguay.png"
+    },
+    {
+        name: "Venezuela",
+        capital: "Caracas",
+        population: 28435943,
+        currency: "Bolívar",
+        language: "Spanish",
+        flag: "assets/images/flags/Venezuela.png"
+    }
+  ];
+  
